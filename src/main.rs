2a709a9e8c714e411f4fec3b7a26a28636069349
gostonefire <peter.steneld@gmail.com>
2025-12-21 15:07:20 +0100

    adds non-tls bind support

--- src/main.rs
+++ src/main.rs
@@ -92,11 +92,17 @@ async fn main() -> Result<(), UnrecoverableError> {
     let ip_addr = Ipv4Addr::from_str(&config.web_server.bind_address).expect("invalid BIND_ADDR");
     let addr = SocketAddr::new(IpAddr::V4(ip_addr), config.web_server.bind_port);
 
-    let rustls_config = RustlsConfig::from_pem_file(&config.web_server.tls_chain_cert, &config.web_server.tls_private_key)
-        .await?;
+    if config.web_server.bind_port == 443 {
+        let rustls_config = RustlsConfig::from_pem_file(&config.web_server.tls_chain_cert, &config.web_server.tls_private_key)
+            .await?;
 
-    tokio::spawn(axum_server::bind_rustls(addr, rustls_config)
-                     .serve(app.into_make_service()));
+        tokio::spawn(axum_server::bind_rustls(addr, rustls_config)
+            .serve(app.into_make_service()));
+    } else {
+        tokio::spawn(axum_server::bind(addr)
+            .serve(app.into_make_service()));
+    }
+
 
     // Main dispatch function
     info!("starting main dispatch function");
