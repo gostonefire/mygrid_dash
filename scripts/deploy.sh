74b21f1c38055e56df8170216c19022e92aeed2d
gostonefire <peter.steneld@gmail.com>
2026-01-19 15:38:07 +0100

    modifies deployment script to use common functions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,84 +13,33 @@ if [ -z "$REPO_NAME" ] || [ -z "$DEV_DIR" ] || [ -z "$SUB_SCRIPT_LOG" ]; then
   exit 1
 fi
 
+# Load common.sh (it is within /usr/local/bin) for common functions
+source common.sh
+
 # Get the owner of the DEV_DIR
 DEV_USER=$(stat -c '%U' "$DEV_DIR")
 
 # Function containing the logic to be run as the directory owner
 run_as_user() {
-  mkdir -p "$HOME/$APP_DIR/config" >> "$SUB_SCRIPT_LOG" 2>&1
-  EXIT_CODE=$?
-  if [ $EXIT_CODE -ne 0 ]; then
-    echo "could not create $HOME/$APP_DIR/config..."
-    exit $EXIT_CODE
-  fi
-
-  mkdir -p "$HOME/$APP_DIR/logs" >> "$SUB_SCRIPT_LOG" 2>&1
-  EXIT_CODE=$?
-  if [ $EXIT_CODE -ne 0 ]; then
-    echo "could not create $HOME/$APP_DIR/logs..."
-    exit $EXIT_CODE
-  fi
-
-  mkdir -p "$HOME/$APP_DIR/last_version" >> "$SUB_SCRIPT_LOG" 2>&1
-  EXIT_CODE=$?
-  if [ $EXIT_CODE -ne 0 ]; then
-    echo "could not create $HOME/$APP_DIR/last_version..."
-    exit $EXIT_CODE
-  fi
-
-  # shellcheck disable=SC2164
-  cd "$DEV_DIR"/"$REPO_NAME" >> "$SUB_SCRIPT_LOG" 2>&1
-  EXIT_CODE=$?
-  if [ $EXIT_CODE -ne 0 ]; then
-    echo "could not change directory to $DEV_DIR"/"$REPO_NAME..."
-    exit $EXIT_CODE
-  fi
+  run_cmd "mkdir -p $HOME/$APP_DIR/config"       "$SUB_SCRIPT_LOG" "could not create $HOME/$APP_DIR/config..."
+  run_cmd "mkdir -p $HOME/$APP_DIR/logs"         "$SUB_SCRIPT_LOG" "could not create $HOME/$APP_DIR/logs..."
+  run_cmd "mkdir -p $HOME/$APP_DIR/last_version" "$SUB_SCRIPT_LOG" "could not create $HOME/$APP_DIR/last_version..."
+  run_cmd "cd $DEV_DIR/$REPO_NAME"               "$SUB_SCRIPT_LOG" "could not change directory to $DEV_DIR/$REPO_NAME..."
 
   if [ -f "$HOME/$APP_DIR/$REPO_NAME" ]; then
-    mv "$HOME/$APP_DIR/$REPO_NAME" "$HOME/$APP_DIR/last_version/"
+    run_cmd "mv $HOME/$APP_DIR/$REPO_NAME $HOME/$APP_DIR/last_version/" "$SUB_SCRIPT_LOG" "could not move $HOME/$APP_DIR/$REPO_NAME to $HOME/$APP_DIR/last_version/"
   fi
 
-  cp "./target/release/$REPO_NAME" "$HOME/$APP_DIR/" >> "$SUB_SCRIPT_LOG" 2>&1
-  EXIT_CODE=$?
-  if [ $EXIT_CODE -ne 0 ]; then
-    echo "could not copy ./target/release/$REPO_NAME to $HOME/$APP_DIR/..."
-    exit $EXIT_CODE
-  fi
+  run_cmd "cp ./target/release/$REPO_NAME $HOME/$APP_DIR/" "$SUB_SCRIPT_LOG" "could not copy ./target/release/$REPO_NAME to $HOME/$APP_DIR/..."
 
   ### Add any extra deploy features to be run as dev user from here ###
-  mkdir -p "$HOME/$APP_DIR/static"
-  rm -rf "$HOME/$APP_DIR/static/"*
-
-  cp -r "./static/." "$HOME/$APP_DIR/static/" >> "$SUB_SCRIPT_LOG" 2>&1
-  EXIT_CODE=$?
-  if [ $EXIT_CODE -ne 0 ]; then
-    echo "could not copy content from ./static to $HOME/$APP_DIR/static/..."
-    exit $EXIT_CODE
-  fi
-
-  cp "./config/config.toml" "$HOME/$APP_DIR/config/" >> "$SUB_SCRIPT_LOG" 2>&1
-  EXIT_CODE=$?
-  if [ $EXIT_CODE -ne 0 ]; then
-    echo "could not copy ./config/config.toml to $HOME/$APP_DIR/config/..."
-    exit $EXIT_CODE
-  fi
-
-  cp "./systemd/$SERVICE_NAME.service" "$HOME/$APP_DIR/" >> "$SUB_SCRIPT_LOG" 2>&1
-  EXIT_CODE=$?
-  if [ $EXIT_CODE -ne 0 ]; then
-    echo "could not copy ./systemd/$SERVICE_NAME.service to $HOME/$APP_DIR/..."
-    exit $EXIT_CODE
-  fi
-
-  cp "./systemd/start.sh" "$HOME/$APP_DIR/" >> "$SUB_SCRIPT_LOG" 2>&1
-  EXIT_CODE=$?
-  if [ $EXIT_CODE -ne 0 ]; then
-    echo "could not copy ./systemd/start.sh to $HOME/$APP_DIR/..."
-    exit $EXIT_CODE
-  fi
-
-  chmod 755 "$HOME/$APP_DIR/start.sh"
+  run_cmd "rm -rf $HOME/$APP_DIR/static"                       "$SUB_SCRIPT_LOG" "could not delete $HOME/$APP_DIR/static..."
+  run_cmd "mkdir -p $HOME/$APP_DIR/static"                     "$SUB_SCRIPT_LOG" "could not create $HOME/$APP_DIR/static..."
+  run_cmd "cp -r ./static/. $HOME/$APP_DIR/static/"            "$SUB_SCRIPT_LOG" "could not copy content from ./static to $HOME/$APP_DIR/static/..."
+  run_cmd "cp ./config/config.toml $HOME/$APP_DIR/config/"     "$SUB_SCRIPT_LOG" "could not copy ./config/config.toml to $HOME/$APP_DIR/config/..."
+  run_cmd "cp ./systemd/$SERVICE_NAME.service $HOME/$APP_DIR/" "$SUB_SCRIPT_LOG" "could not copy ./systemd/$SERVICE_NAME.service to $HOME/$APP_DIR/..."
+  run_cmd "cp ./systemd/start.sh $HOME/$APP_DIR/"              "$SUB_SCRIPT_LOG" "could not copy ./systemd/start.sh to $HOME/$APP_DIR/..."
+  run_cmd "chmod 755 $HOME/$APP_DIR/start.sh"                  "$SUB_SCRIPT_LOG" "could not make start script executable"
 
   ### until to here! ##################################################
 }
@@ -111,20 +60,8 @@ if [ $EXIT_CODE -ne 0 ]; then
 fi
 
 ########## From here the script will be run as root, so add any commands such as systemctl etc. from here ##########
-cp "$HOME/$APP_DIR/$SERVICE_NAME.service" /lib/systemd/system/ >> "$SUB_SCRIPT_LOG" 2>&1
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-  echo "failed to copy $SERVICE_NAME.service to /lib/systemd/system/..."
-  exit $EXIT_CODE
-fi
-
-systemctl daemon-reload
-
-systemctl enable --now "$SERVICE_NAME.service" >> "$SUB_SCRIPT_LOG" 2>&1
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-  echo "could not enable and start $SERVICE_NAME.service..."
-  exit $EXIT_CODE
-fi
+run_cmd "cp $HOME/$APP_DIR/$SERVICE_NAME.service /lib/systemd/system/" "$SUB_SCRIPT_LOG" "failed to copy $SERVICE_NAME.service to /lib/systemd/system/..."
+run_cmd "systemctl daemon-reload"                                      "$SUB_SCRIPT_LOG" "failed to reload systemd daemon"
+run_cmd "systemctl enable --now $SERVICE_NAME.service"                 "$SUB_SCRIPT_LOG" "could not enable and start $SERVICE_NAME.service..."
 
 ########## until to here! ##########################################################################################
